321e671ab8e492d91458d467dd262aef536854b1
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2023-01-31 17:36:09 -0500

    Add files via upload

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,5 +1,43 @@
-function Dropdown({ options }) {
-  return <div>Dropdown...</div>;
+import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+import Panel from "./Panel";
+
+function Dropdown({ options, value, onChange }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    // close dropdown
+    setIsOpen(false);
+    //what option did the user click on
+    onChange(option);
+  };
+  const renderedOptions = options.map((option) => {
+    return (
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-3"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div className="w-48 relative">
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg" />
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
+    </div>
+  );
 }
 
 export default Dropdown;
