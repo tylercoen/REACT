1166640f021003d693225c78d2cc5078dd8dd680
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2023-02-03 15:54:37 -0500

    Add files via upload

---
+++ comps/src/components/Table.js
@@ -0,0 +1,24 @@
+function Table({ data }) {
+  const renderedRows = data.map((fruit) => {
+    return (
+      <tr key={fruit.name}>
+        <td>{fruit.name}</td>
+        <td>{fruit.color}</td>
+        <td>{fruit.score}</td>
+      </tr>
+    );
+  });
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
+}
+export default Table;
