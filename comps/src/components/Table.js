6de583f56612a5674ed14e34a5213b3d3c9af4a8
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2023-02-06 13:41:11 -0500

    Add files via upload

--- comps/src/components/Table.js
+++ comps/src/components/Table.js
@@ -1,21 +1,27 @@
-function Table({ data }) {
-  const renderedRows = data.map((fruit) => {
+import { render } from "@testing-library/react";
+
+function Table({ data, config, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column) => {
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(rowData)}
+        </td>
+      );
+    });
     return (
-      <tr key={fruit.name}>
-        <td>{fruit.name}</td>
-        <td>{fruit.color}</td>
-        <td>{fruit.score}</td>
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
       </tr>
     );
   });
   return (
-    <table>
+    <table className="table-auto border-spacing-2">
       <thead>
-        <tr>
-          <th>Fruit</th>
-          <th>Color</th>
-          <th>Score</th>
-        </tr>
+        <tr className="border-b-2">{renderedHeaders}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
