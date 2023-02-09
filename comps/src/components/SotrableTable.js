21bac9a5da717a80bd210694fb653732ed690aa6
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2023-02-09 17:45:30 -0500

    Add files via upload

---
+++ comps/src/components/SotrableTable.js
@@ -0,0 +1,6 @@
+import Table from "./Table";
+function SortableTable(props) {
+  return <Table {...props} />;
+}
+
+export default SortableTable;
--- comps/src/pages/TablePage.js
+++ comps/src/pages/TablePage.js
@@ -1,4 +1,4 @@
-import Table from "../components/Table";
+import SortableTable from "../components/SotrableTable";
 
 function TablePage() {
   const data = [
@@ -13,7 +13,11 @@ function TablePage() {
       label: "Color",
       render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
     },
-    { label: "Score", render: (fruit) => fruit.score },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      header: () => <th className="bg-red-500">Score</th>,
+    },
   ];
 
   const keyFn = (fruit) => {
@@ -21,7 +25,7 @@ function TablePage() {
   };
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
