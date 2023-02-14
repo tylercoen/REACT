b8d61a9a3587c4e3b0e226a0de719b1cbd42f3c3
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2023-02-14 16:42:46 -0500

    Add files via upload

--- comps/src/pages/CounterPage.js
+++ comps/src/pages/CounterPage.js
@@ -1,3 +1,4 @@
+import produce from "immer";
 import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
@@ -10,20 +11,32 @@ const ADD_VALUE_TO_COUNT = "add-value-to-count";
 const reducer = (state, action) => {
   switch (action.type) {
     case INCREMENT_COUNT:
-      return { ...state, count: state.count + 1 };
+      state.count = state.count + 1;
+      return;
+    //without immer vvvvv, make sure you return the state first then change it
+    //return { ...state, count: state.count + 1 };
     case DECREMENT_COUNT:
-      return { ...state, count: state.count - 1 };
+      state.count = state.count - 1;
+      return;
+    //return { ...state, count: state.count - 1 };
     case SET_VALUE_TO_ADD:
-      return { ...state, valueToAdd: action.payload };
+      state.valueToAdd = action.payload;
+      return;
+    //return { ...state, valueToAdd: action.payload };
     case ADD_VALUE_TO_COUNT:
-      return { ...state, count: state.count + state.valueToAdd, valueToAdd: 0 };
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
+    //return { ...state, count: state.count + state.valueToAdd, valueToAdd: 0 };
     default:
-      return state;
+      return;
+    //without immer you have to return state.
+    //return state;
   }
 };
 
 function CounterPage({ initialCount }) {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   });
