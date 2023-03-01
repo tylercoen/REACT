0a98348195f29887967bd831ab7d4c34f925ba48
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2023-02-28 19:54:22 -0500

    Add files via upload

---
+++ media/src/store/index.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { usersReducer } from "./slices/usersSlice";
+
+export const store = configureStore({
+  reducer: { users: usersReducer },
+});
