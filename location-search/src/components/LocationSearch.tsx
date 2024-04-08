7bc4bf0d97dbcb327f1dce6a5041723311a94188
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2024-04-08 18:44:50 -0400

    Add files via upload
    Initiating location-search project.

---
+++ location-search/src/components/LocationSearch.tsx
@@ -0,0 +1,31 @@
+import type { Place } from "../api/Place";
+import { useState } from "react";
+import { search } from "../api/search";
+
+interface LocationSearchProps {
+  onPlaceClick: (place: Place) => void;
+}
+
+export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
+  const [places, setPlaces] = useState<Place[]>([]);
+  const [term, setTerm] = useState("");
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await search(term);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className="font-bold" htmlFor="term">
+          Search
+        </label>
+        <input
+          className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full"
+          id="term"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        ></input>
+      </form>
+    </div>
+  );
+}
---
+++ location-search/src/components/Map.tsx
@@ -0,0 +1,9 @@
+import type { Place } from "../api/Place";
+
+interface MapProps {
+  place: Place | null;
+}
+
+export default function Map({ place }: MapProps) {
+  return <div>Map!</div>;
+}
