4d591c576e46ed39643510fa7a1e1e2692aef51c
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2024-04-09 15:54:19 -0400

    Add files via upload

--- location-search/src/components/LocationSearch.tsx
+++ location-search/src/components/LocationSearch.tsx
@@ -1,5 +1,5 @@
 import type { Place } from "../api/Place";
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { search } from "../api/search";
 
 interface LocationSearchProps {
@@ -11,7 +11,8 @@ export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
   const [term, setTerm] = useState("");
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    await search(term);
+    const results = await search(term);
+    setPlaces(results);
   };
   return (
     <div>
@@ -26,6 +27,24 @@ export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
           onChange={(e) => setTerm(e.target.value)}
         ></input>
       </form>
+
+      <h1 className="font-bold mt-6">Found Locations</h1>
+      <div className="grid grid-cols-[1fr_40px] gap-2 mt-2 items-center">
+        {places.map((place) => {
+          return (
+            <Fragment key={place.id}>
+              <p className="text-sm">{place.name}</p>
+              <button
+                className="bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded"
+                onClick={() => onPlaceClick(place)}
+              >
+                Go
+              </button>
+              <div className="border-b w-full col-span-2" />
+            </Fragment>
+          );
+        })}
+      </div>
     </div>
   );
 }
